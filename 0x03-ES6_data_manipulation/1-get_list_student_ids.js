f502c86f500e6974ae451d75339ec693b0e49e83
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-13 20:52:13 +0300

    Task1[Edit]: More mapping {fix nameing and exporting}

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
-export default function (students) {
+const getListStudentIds = (students) => {
   if (!Array.isArray(students)) return [];
   return students.map((students) => students.id);
-}
+};
+
+export default getListStudentIds;

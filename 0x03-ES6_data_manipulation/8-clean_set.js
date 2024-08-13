2e7a2d2ce6947684eab1168cc33cd46038429d4a
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-13 23:11:27 +0300

    Task8[Edit]: Clean set {fix the constrains}

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 const cleanSet = (set, startString) => {
-  if (!startString) return '';
+  if (!startString || typeof startString !== 'string') return '';
   const result = [];
   set.forEach((item) => {
     if (item.startsWith(startString)) result.push(item.slice(startString.length));

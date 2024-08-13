14443c596e98492077dae30c56de6233cb2b07b9
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-13 23:43:35 +0300

    Task11[Add]: Weak link data structure

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export const queryAPI = (endpoint) => {
   const endpointNums = weakMap.get(endpoint) || 0;
-  if (endpointNums >= 5) throw new Error('Endpoint load is high');
+  if (endpointNums >= 4) throw new Error('Endpoint load is high');
 
   weakMap.set(endpoint, endpointNums + 1);
 };

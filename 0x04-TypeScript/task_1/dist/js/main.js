4cad8ee4459823a4af29dd47e7f027df1ea68fc8
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-16 22:48:11 +0300

    Compile task_1 folder

---
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -0,0 +1,34 @@
+var teacher3 = {
+    firstName: 'Nabil',
+    fullTimeEmployee: false,
+    lastName: 'Ragab',
+    location: 'Cairo',
+    contract: false,
+};
+console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
+var printTeacher = function (firstName, lastName) {
+    return "".concat(firstName.slice(0, 1), ". ").concat(lastName);
+};
+printTeacher("John", "Doe");
+var StudentClass = /** @class */ (function () {
+    function StudentClass(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    StudentClass.prototype.workOnHomework = function () {
+        return "Currently working";
+    };
+    StudentClass.prototype.displayName = function () {
+        return this.firstName;
+    };
+    return StudentClass;
+}());
+//# sourceMappingURL=main.js.map

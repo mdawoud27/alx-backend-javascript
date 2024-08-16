08071f8c42f372888d60a1467c6fcb473d108093
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-16 22:29:14 +0300

    Task5[Add]: Advanced types Part 1

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,41 @@
+// 5. Advanced types Part 1
+interface DirectorInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+        return "Working from home";
+    }
+    getCoffeeBreak(): string {
+        return "Getting a coffee break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to director tasks";
+    }
+}
+
+class Teacher implements TeacherInterface {
+    workFromHome(): string {
+        return "Cannot work from home";
+    }
+    getCoffeeBreak(): string {
+        return "Cannot have a break";
+    }
+    workDirectorTasks(): string {
+        return "Getting to work";
+    }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+    if (typeof salary === 'number' && salary < 500) return new Teacher();
+    else return new Director();
+}

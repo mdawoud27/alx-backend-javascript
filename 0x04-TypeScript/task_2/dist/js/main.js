a64c7a0502cd252bf148d319b270fe88e5748dae
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-08-16 22:47:37 +0300

    Compile task_2 folder

---
+++ 0x04-TypeScript/task_2/dist/js/main.js
@@ -0,0 +1,50 @@
+var Director = /** @class */ (function () {
+    function Director() {
+    }
+    Director.prototype.workFromHome = function () {
+        return "Working from home";
+    };
+    Director.prototype.getCoffeeBreak = function () {
+        return "Getting a coffee break";
+    };
+    Director.prototype.workDirectorTasks = function () {
+        return "Getting to director tasks";
+    };
+    return Director;
+}());
+var Teacher = /** @class */ (function () {
+    function Teacher() {
+    }
+    Teacher.prototype.workFromHome = function () {
+        return "Cannot work from home";
+    };
+    Teacher.prototype.getCoffeeBreak = function () {
+        return "Cannot have a break";
+    };
+    Teacher.prototype.workTeacherTasks = function () {
+        return "Getting to work";
+    };
+    return Teacher;
+}());
+function createEmployee(salary) {
+    if (typeof salary === 'number' && salary < 500)
+        return new Teacher();
+    else
+        return new Director();
+}
+// 6. Creating functions specific to employees
+function isDirector(employee) {
+    return employee.workDirectorTasks() !== undefined;
+}
+function executeWork(employee) {
+    if (isDirector(employee)) {
+        console.log(employee.workDirectorTasks());
+    }
+    else {
+        console.log(employee.workTeacherTasks());
+    }
+}
+function teachClass(todayClass) {
+    return "Teaching ".concat(todayClass === 'Math' ? 'Math' : 'History');
+}
+//# sourceMappingURL=main.js.map

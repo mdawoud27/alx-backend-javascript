0dfed1addd4051ad5e308ddd6d4c0fb7e3eeb02f
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-10-29 10:53:09 +0300

    Task2[Add]: Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,44 @@
+const fs = require("fs");
+
+const getDataFromCsvFile = (path) => {
+  const csvData = fs.readFileSync(path, "utf8");
+  const rows = csvData.trim().split("\n");
+  return rows.slice(1).map((row) => row.split(","));
+};
+
+const assignNamesWithFields = (results) => {
+  const fieldsWithFirstName = {};
+
+  results.forEach((entry) => {
+    const field = entry[3];
+    const firstName = entry[0];
+
+    if (!fieldsWithFirstName[field]) {
+      fieldsWithFirstName[field] = [];
+    }
+    fieldsWithFirstName[field].push(firstName);
+  });
+
+  return fieldsWithFirstName;
+};
+
+const printFieldStats = (fieldWithName) => {
+  for (const [key, value] of Object.entries(fieldWithName)) {
+    const list = value.join(", ");
+    console.log(`Number of students in ${key}: ${value.length}. List: ${list}`);
+  }
+};
+
+const countStudents = (path) => {
+  try {
+    const results = getDataFromCsvFile(path);
+    const fieldsWithFirstName = assignNamesWithFields(results);
+
+    console.log(`Number of students: ${results.length}`);
+    printFieldStats(fieldsWithFirstName);
+  } catch (error) {
+    throw new Error("Cannot load the database");
+  }
+};
+
+module.exports = countStudents;

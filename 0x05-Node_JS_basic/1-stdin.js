e0bc014b7f82c344d113ad431de5a520f97e777a
Mohamed Dawoud <mhmaddawoud20@gmail.com>
2024-10-29 11:08:09 +0300

    Task1[Edit]: Using Process stdin {fix output syntax}

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,5 +5,5 @@ process.stdin.on("data", (data) => {
 });
 
 process.stdin.on("end", () => {
-  process.stdout.write("This important software is now closing,");
+  process.stdout.write("This important software is now closing\n");
 });
